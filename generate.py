2c486b2f64a3ee7d9e2528a2257ca1643f18f3f1
locch <locc7196@gmail.com>
2024-10-30 20:01:22 +0700

    update

---
+++ generate.py
@@ -0,0 +1,32 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import torch
+# Check if a GPU is available
+if torch.cuda.is_available():
+    # Get the current device index (default is 0 if no other device is specified)
+    current_device = torch.cuda.current_device()
+    
+    # Get the name of the GPU at this device index
+    gpu_name = torch.cuda.get_device_name(current_device)
+    print(f"Current GPU: {gpu_name}")
+else:
+    print("No GPU available.")
+
+
+from transformers import pipeline
+
+model_id = "fine-tuned-model"
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    device_map="auto",
+)
+messages = [
+    {"role": "user", "content": "create the boolean syntax to find the accounting intern profiles on Linkedin through Google"},
+]
+outputs = pipe(
+    messages,
+    max_new_tokens=128
+)
+print(outputs[0]["generated_text"][-1]["content"])
